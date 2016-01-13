386d29a5553dae618bd7eb03882493bdca068557
tezter <digizaf@gmail.com>
2016-01-13 23:22:24 +0100

    oca stuff

---
+++ encap/Tester.java
@@ -0,0 +1,11 @@
+public class Tester {
+
+public static void main(String[] args) {
+
+Time time = new Time();
+time.hour = 12;
+time.minute = 55;
+time.second = 30;
+time.displayTime();
+}
+}
---
+++ encap/Time.java
@@ -0,0 +1,10 @@
+public class Time {
+
+int hour;
+int minute;
+int second;
+
+public void displayTime() {
+System.out.println("The time is: " + hour + ":" + minute + ":" + second);
+}
+}
