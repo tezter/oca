f6ea1c5c846dc0913509228eeb04a0cbbe3ec4a4
zpolat <zafer.polat@ebpi.nl>
2016-01-14 16:03:40 +0100

    constructor

--- encap/Tester.java
+++ encap/Tester.java
@@ -1,10 +1,17 @@
+import java.lang.*;
+
 public class Tester {
 
 public static void main(String[] args) {
 
-Time time = new Time();
+Time time = new Time(3,15);
+//Time time = new Time();
 //int hour;
-time.setHour(12);
+try{
+time.setHour(13);
+}catch(Exception e){
+ System.out.println("translated naar normale taal : invalid input");
+}
 //hour = time.getHour();
 //System.out.println(hour);
 time.displayTime(); //  roept method aan
--- encap/Time.java
+++ encap/Time.java
@@ -1,17 +1,42 @@
+
+import java.lang.*;
 public class Time {
+
+
+public Time(int k,int m){
+
+}
+
+//Time a = new Time(3,15); 
+//Time b = a;
+
+
 
 private int hour;
 public int minute = 1;
 private int second; //  waarom gaat dit goed in displayTime()??
+//private char ery = "error"
 
 public void displayTime() {
 System.out.println("The time is: " + hour + ":" + minute + ":" + second);
 }
 public void setHour(int h){
+System.out.println("1");
+if (h >12){
+System.out.println("2");
+throw new IllegalArgumentException("input is invalid");
+}
+//if (h <12){
 hour = h;
+//}
+//else {
+//hour = 31337;
+//ery = hour;
+//}
 }
 public int getHour() {
 return hour;
 }
 
 }
+
